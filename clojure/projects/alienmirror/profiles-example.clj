5955aedc5fe32361a6a8d2ec76d7db9832ca08e3
Jalal Fathi <jfathi.mail@gmail.com>
2017-02-25 17:12:35 +0100

    added profiles example

---
+++ clojure/projects/alienmirror/profiles-example.clj
@@ -0,0 +1,3 @@
+{:dev {:env {:page-access-token "EAATLG6tKBfwBAKtsuz0vYTZAtl4cqEMUeFh8am63CnVBIZC1yw612IaUsQugONkZADXNJhlZBTCFZCEd9sdADyVT5zP6eXJMZA2MCDqNlAEC9ZB7tcZBbPoh10JSPZALtDePvk7EV3VP0PLeiA7my02zAyZCNASO6LoQZBcWG9ZCupZBIaQZDZD"
+             :verify-token "ABC"
+             :vision-key "AIzaSyAwUKV0eQha3wrhyvJm2KclDs9K1_jKh18"}}}
