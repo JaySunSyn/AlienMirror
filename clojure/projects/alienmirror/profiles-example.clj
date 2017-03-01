8c2d47d8a091886e0036833db9e1efbf33ac4e97
Jalal Fathi <jfathi.mail@gmail.com>
2017-03-01 12:50:20 +0000

    cleaned up profile example

--- clojure/projects/alienmirror/profiles-example.clj
+++ clojure/projects/alienmirror/profiles-example.clj
@@ -1,3 +1,3 @@
-{:dev {:env {:page-access-token "EAATLG6tKBfwBAKtsuz0vYTZAtl4cqEMUeFh8am63CnVBIZC1yw612IaUsQugONkZADXNJhlZBTCFZCEd9sdADyVT5zP6eXJMZA2MCDqNlAEC9ZB7tcZBbPoh10JSPZALtDePvk7EV3VP0PLeiA7my02zAyZCNASO6LoQZBcWG9ZCupZBIaQZDZD"
-             :verify-token "ABC"
-             :vision-key "AIzaSyAwUKV0eQha3wrhyvJm2KclDs9K1_jKh18"}}}
+{:dev {:env {:page-access-token "PAGE_ACCESS_TOKEN"
+             :verify-token "A_TOKEN"
+             :vision-key "VISION_API_KEY"}}}
